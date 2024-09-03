7b7c5abfbef98c5a66e540dcfad11daa92661017
mdemong87 <mdemong87@gmail.com>
2024-09-03 15:04:22 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+import ConnectDB from '@/lib/connectionDB'
 import { Inter } from 'next/font/google'
 import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Picture TV',
@@ -9,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
-
+  ConnectDB();
 
   return (
     <html lang="en">
