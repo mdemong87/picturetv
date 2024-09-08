4bfafd0dbbd1cb5de85d9401d2f2ca60cc184e13
mdemong87 <mdemong87@gmail.com>
2024-09-08 23:28:49 +0600

    update

--- app/componnent/clientcomponnent/searchResultpage/SearchPageCardWrper.jsx
+++ app/componnent/clientcomponnent/searchResultpage/SearchPageCardWrper.jsx
@@ -1,24 +1,27 @@
-import ImageCard from "../imageCard";
 
 const SearchPageCardWrper = () => {
     return (
-        <div className="w-full grid items-center gap-5 grid-cols-12">
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
-            <ImageCard link={'/searchresult/vedioorimage'} />
+        // <div className="w-full grid items-center gap-5 grid-cols-12">
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        //     <ImageCard link={'/searchresult/vedioorimage'} />
+        // </div>
+
+        <div>
+            <h1 className="text-center text-4xl text-red-500">This website is Under Developing. We Will Fixed All Issues Very soon.</h1>
         </div>
     )
 }
