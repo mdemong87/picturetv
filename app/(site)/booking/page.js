83cdab18756757fcc9262d2ce46a7c223b80fd24
mdemong87 <mdemong87@gmail.com>
2024-10-07 22:25:19 +0600

    remove from under development of booking page

--- app/(site)/booking/page.js
+++ app/(site)/booking/page.js
@@ -1,7 +1,6 @@
 'use client'
 
 import Container from "@/app/componnent/clientcomponnent/Container";
-import UnderDevelopment from "@/app/componnent/clientcomponnent/UnderDevelopment";
 import Five from "@/app/componnent/clientcomponnent/bookingpage/Five";
 import Four from "@/app/componnent/clientcomponnent/bookingpage/Four";
 import NextAndPrev from "@/app/componnent/clientcomponnent/bookingpage/NextAndPrev";
@@ -36,9 +35,8 @@ const Bookign = () => {
                     </Container>
                 </div>
 
-            </div>
 
-            <UnderDevelopment />
+            </div>
         </main>
     )
 }
