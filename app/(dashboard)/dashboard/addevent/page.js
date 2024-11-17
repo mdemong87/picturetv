23e1aff4d4392d43a71fe8ffdab3689934e7ddc4
mdemong87 <mdemong87@gmail.com>
2024-11-17 16:08:29 +0600

    check

--- app/(dashboard)/dashboard/addevent/page.js
+++ app/(dashboard)/dashboard/addevent/page.js
@@ -1,6 +1,6 @@
 import Container from "@/app/componnent/clientcomponnent/Container";
 import AddEventWper from "@/app/componnent/deshboardcomponnent/addevent/AddEventWrper";
-import gertusersession from "@/lib/helper/getusersesssion";
+// import gertusersession from "@/lib/helper/getusersesssion";
 import { ToastContainer } from "react-toastify";
 
 
@@ -9,13 +9,13 @@ async function AddEvent() {
 
 
 
-    const session = await gertusersession();
+    // const session = await gertusersession();
 
 
     return (
         <main className="h-fit pb-10">
             <Container>
-                <AddEventWper session={session} />
+                <AddEventWper session={""} />
             </Container>
             <ToastContainer />
         </main>
